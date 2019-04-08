6f3c53723850dacfe30985ccef9dd19febd52195
Nick Spinosa <nickspinosa1022@gmail.com>
2019-04-08 12:43:13 -0400

    .doom.d config

---
+++ config.el
@@ -0,0 +1,11 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;; Place your private configuration here
+;;
+(setq company-idle-delay 0.2
+      company-minimum-prefix-length 3)
+
+;; defaults
+(setq doom-font (font-spec :family "Fira Mono" :size 18)
+      doom-variable-pitch-font (font-spec :family "Fira Sans")
+      doom-unicode-font (font-spec :family "DejaVu Sans Mono")
+      doom-big-font (font-spec :family "Fira Mono" :size 27))
